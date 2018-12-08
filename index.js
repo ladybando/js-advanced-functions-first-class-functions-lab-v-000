6c0ad44c3b7de6ee608a503339c486e227ea3280
Ayana Bando <ayanaflax@yahoo.com>
2018-12-08 20:13:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers){
+  let firstDrive = drivers.slice(0,2);
+  return firstDrive;
+};
+
+const returnLastTwoDrivers = function(drivers){
+  let lastDrive = drivers.slice(-2);
+  return lastDrive;
+};
+
+const selectingDrivers = function() {
+    return returnFirstTwoDrivers();
+    return returnLastTwoDrivers();
+      
+    };
